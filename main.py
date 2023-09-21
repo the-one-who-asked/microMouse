4a86da344a79603e78b21ee4837e73141647203c
the-one-who-asked <ansh.arora0512@gmail.com>
2023-09-21 21:51:25 +0200

    Git test

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+class Mouse:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.walls = [[0 for _ in range(15)] for _ in range(15)]
