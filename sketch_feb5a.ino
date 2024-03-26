bfb133c3777a4a6e5e42d16fa8b71e7d561ecfa3
the-one-who-asked <115102671+the-one-who-asked@users.noreply.github.com>
2024-03-26 21:26:14 +0100

    Update sketch_feb5a.ino

--- sketch_feb5a.ino
+++ sketch_feb5a.ino
@@ -4,195 +4,47 @@
 #include <Wire.h>
 
 /*---------------------------- Data Types ----------------------------*/
-// Aliases bytes as coordinates to help distinguish between walls and coordinates, both of which are stored as bytes
-typedef byte C;
+typedef byte C; // Aliases bytes as coordinates to help distinguish between walls and coordinates, both of which are stored as bytes
 
 /*--------------------------- Declarations ---------------------------*/
 // Variables
 byte walls[16][16];
 int orient = 0;
 C pos = 0x00;
-float Ax, Ay, Gz, Dw, Dn, De;
+float Ax, Ay, Gz, D[3];
 sensors_event_t Acc, Gyro, Temp;
 
 // Constants
-const int MOTOR_OUTPUT, trigPin, echoPin;
-const float PI = 3.141593;
-const float HALF_PI = 1.570796;
-trigPin = 3;
-echoPin = 2;
+uint8_t trig[3], echo[3];
+const float square_width;
 
-/*---------------------------- Functions -----------------------------*/
-// Checks whether a coordinate is in an array of coordinates
-bool in(C coord, C* coords, int size) {
-  for (int i = 0; i < size; ++i) {
-    if (coord == coords[i]) {
-      return true;
-    }
-  }
-  return false;
-}
+/*-------------------------- Input Modules ---------------------------*/
+void scan(int direction) {
 
-// Roughly rotates the mouse to allign with a given orientation
-void reorient(C new_pos) {
-  // Check gyroscope
+  // A clean HIGH pulse is sent to the chosen ultrasonic sensor, triggering it to emit sound
+  digitalWrite(trig[direction], LOW);
+  delayMicroseconds(5);
+  digitalWrite(trig[direction], HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig[direction], LOW);
 
-  const C old_pos = pos;
-  pos = new_pos;
-
-  // Orientate north
-  if (old_pos + 0x10 == new_pos) {
-    if ((orient == 0) && (Gz < 0.1 || Gz > 2 * PI - 0.1)) return;
-    bool disoriented() {return Gz < PI};
-    orient = 0;
-
-  // Orientate east
-  } else if (old_pos + 0x01 == new_pos) {
-    if ((orient == 1) && (abs(Gz - HALF_PI) < 0.1)) return;
-    bool disoriented() {return Gz < HALF_PI || Gz > 3 * HALF_PI};
-    orient = 1;
-
-  // Orientate south
-  } else if (old_pos - 0x10 == new_pos) {
-    if ((orient == 2) && (abs(Gz - PI) < 0.1)) return;
-    bool disoriented() {return Gz > PI};
-    orient = 2;
-
-  // Orientate west
-  } else {
-    if ((orient == 3) && (abs(Gz - HALF_PI * 3) < 0.1)) return;
-    bool disoriented() {Gz > HALF PI && Gz < 3 * HALF_PI};
-    orient = 3;
-  }
-
-  // Rotates clockwise or anticlockwise until the orientation is a certain value
-  if (disoriented()) {
-    // Reverse left motor
-    // Activate motors
-    while (disoriented()) {
-      delay(10)
-      // Check gyroscope
-    }
-    // Reverse left motor
-  } else {
-    // Reverse right motor
-    // Activate motors
-    while (!disoriented()) {
-      delay(10)
-      // Check gyroscope
-    }
-    // Reverse right motor
-  }
-}
-
-// Places the mouse ot the centre of the next square
-void centre() {
-  // Check all sensors
-
-  // Calculates distances to each wall
-  float phi, tempn, Sn;
-  const float cos_phi, h, projected, tempw, Sw, tempe, Se;
-  phi = Gz - orient * HALF_PI;
-  cos_phi = cos(phi);
-  tempn = Dn * cos_phi;
-  Sn = tempn - floor(tempn)
-  h = Sn + 0.5;
-  tempw = Dw * cos_phi;
-  Sw = tempw - floor(tempw);
-  tempe = De * cos_phi;
-  Se = tempe - floor(tempe);
-  projected = h * tan(phi) + Sw - Se;
-
-  // Course-corrects the mouse if its going too far right
-  if (projected > 0.1) {
-    const float w, s, theta, inv_r;
-    w = abs(Sw - Se);
-    s = sqrt(h**2 + w**2);
-    theta = acos((h*cos_phi + h - w*sin(Gz))/s) + acos(-h/s) + PI;
-    if (theta > 2 * PI) theta -= 2 * PI;
-    inv_r = (1 + cos_phi + 2 * cos(theta)) / w;
-
-    // Output MOTOR_OUTPUT to right motor
-    // Output MOTOR_OUTPUT - inv_r to left motor
-    while (phi > theta || phi < PI) {
-      delay(10);
-      // Check gyroscope
-    }
-    // Output MOTOR_OUTPUT to left motor
-    // Output MOTOR_OUTPUT - inv_R to right motor
-    while (phi > PI) {
-      delay(10);
-      // Check gyroscope
-    }
-    // Output MOTOR_OUTPUT to right motor
-  }
-
-  // Course-corrects the mouse if its going too far left
-  else if (projected < -0.1) {
-    const float w, s, theta, inv_r;
-    w = abs(Sw - Se);
-    s = sqrt(h**2 + w**2);
-    theta = acos((h*cos_phi + h - w*sin(Gz))/s) + acos(-h/s) + PI;
-    if (theta > 2 * PI) theta -= 2 * PI;
-    inv_r = (1 + cos_phi + 2 * cos(theta)) / w;
-
-    // Output MOTOR_OUTPUT to left motor
-    // Output MOTOR_OUTPUT - inv_r to right motor
-    while (phi < theta || phi > PI) {
-      delay(10);
-      // Check gyroscope
-      phi = Gz - orient * HALF_PI;
-    }
-    // Output MOTOR_OUTPUT to right motor
-    // Output MOTOR_OUTPUT - inv_R to left motor
-    while (phi < PI) {
-      delay(10);
-      // Check gyroscope
-      phi = Gz - orient * HALF_PI;
-    }
-    // Output MOTOR_OUTPUT to right motor
-  }
-
-  // Brings the mouse directly to the centre of the next square along a straight line
-  else {
-    // Output MOTOR_OUTPUT to right motor
-    // Output MOTOR_OUTPUT to left motor
-    if (Sn > 0.5) {
-      while (Sn > 0.4) {
-        delay(10);
-        // Check north ultrasonic sensor
-        tempn = Dn * cos_phi;
-        Sn = tempn - floor(tempn);
-      }
-    }
-    while (Sn < 0.5) {
-      delay(10);
-      // Check north ultrasonic sensor
-      tempn = Dn * cos_phi;
-      Sn = tempn - floor(tempn);
-    }
-    while (Sn > 0.5) {
-      delay(10);
-      // Check north ultrasonic senor
-      tempn = Dn * cos_phi;
-      Sn = tempn - floor(tempn);
-    }
-  }
+  // Calculates distance in squares by recording the time it takes for the sound to echo back to the sensor
+  D[direction] = pulseIn(echo[direction], HIGH) * 0.01715 / square_width;
 }
 
 void update_walls() {
-  // Check all sensors
+  for (int i = 0; i < 3; i++) {scan(i);}
+  // Check gyroscope
 
   // Calculates distance to each wall
-  const float cos_phi;
-  const int Sw, Sn, Se, n, e, s, w;
+  float cos_phi = cos(Gz - orient * HALF_PI);
+  int Sw, Sn, Se, n, e, s, w, temp;
   const int y = pos >> 4;
   const int x = pos & 0x0F;
-  int temp;
   cos_phi = cos(Gz - orient * HALF_PI);
-  Sw = Dw * cos_phi;
-  Sn = Dn * cos_phi;
-  Se = De * cos_phi;
+  Sw = D[0] * cos_phi;
+  Sn = D[1] * cos_phi;
+  Se = D[2] * cos_phi;
 
   // Updates walls array accordingly
   if (orient == 0) {
@@ -247,6 +99,16 @@ void update_walls() {
     walls[temp][x] /= 0x08;
     if (temp != 15) walls[temp + 1][x] /= 0x02;
   }
+}
+
+/*------------------------ Processing Modules ------------------------*/
+bool in(C coord, C* coords, int size) {
+  for (int i = 0; i < size; ++i) {
+    if (coord == coords[i]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // Calculates the fastest path from the mouse to the destination based on the information found by the mouse, returns the first coordinate on that path
@@ -327,26 +189,216 @@ C floodfill(C pos) {
   delete[] prev_source;
 }
 
-/*------------------------------- Main -------------------------------*/
-void setup() {
-  Serial.begin(9600);
+/*-------------------------- Output Modules --------------------------*/
+void reorient(C new_pos) {
+  // Check gyroscope
 
-  // Initialises the grid of walls
-  for (int i = 0; i < 16; ++i) {
-    for (int j = 0; j < 16; ++j) {
-      walls[i][j] = B0;
+  const C old_pos = pos;
+  pos = new_pos;
+
+  // Orientate north
+  if ((old_pos + 0x10 == new_pos) && (Gz > 0.1) && (Gz < 2 * PI - 0.1)) {
+    orient = 0;
+    if (Gz < PI) {
+      // Reverse left motor
+      // Activate motors
+      while (Gz < PI) {
+        delayMicroseconds(10);
+        // Check gyroscope
+      }
+      // Reverse left motor
+    } else {
+      // Reverse right motor
+      // Activate motors
+      while (Gz > PI) {
+        delayMicroseconds(10);
+        // Check gyroscope
+      }
+      // Reverse right motor
+    }
+
+  // Orientate east
+  } else if ((old_pos + 0x01 == new_pos) && (abs(Gz - HALF_PI) > 0.1)) {
+    orient = 1;
+    if (Gz < HALF_PI || Gz > 3 * HALF_PI) {
+      // Reverse left motor
+      // Activate motors
+      while (Gz < HALF_PI || Gz > 3 * HALF_PI) {
+        delayMicroseconds(10);
+        // Check gyroscope
+      }
+      // Reverse left motor
+    } else {
+      // Reverse right motor
+      // Activate motors
+      while ((Gz > HALF_PI) && (Gz < 3 * HALF_PI)) {
+        delayMicroseconds(10);
+        // Check gyroscope
+      }
+      // Reverse right motor
+    }
+
+  // Orientate south
+  } else if ((old_pos - 0x10 == new_pos) && (abs(Gz - PI) > 0.1)) {
+    orient = 2;
+    if (Gz > PI) {
+      // Reverse left motor
+      // Activate motors
+      while (Gz > PI) {
+        delayMicroseconds(10);
+        // Check gyroscope
+      }
+      // Reverse left motor
+    } else {
+      // Reverse right motor
+      // Activate motors
+      while (Gz < PI) {
+        delayMicroseconds(10);
+        // Check gyroscope
+      }
+      // Reverse right motor
+    }
+
+  // Orientate west
+  } else if (abs(Gz - HALF_PI * 3) > 0.1) {
+    orient = 3;
+    if (Gz > HALF_PI && Gz < 3 * HALF_PI) {
+      // Reverse left motor
+      // Activate motors
+      while (Gz > HALF_PI && Gz < 3 * HALF_PI) {
+        delayMicroseconds(10);
+        // Check gyroscope
+      }
+      // Reverse left motor
+    } else {
+      // Reverse right motor
+      // Activate motors
+      while (Gz < HALF_PI || Gz > 3 * HALF_PI) {
+        delayMicroseconds(10);
+        // Check gyroscope
+      }
+      // Reverse right motor
     }
   }
 }
 
-void loop() {
-  C end[4];
-  end[0] = 0x77;
-  end[1] = 0x78;
-  end[2] = 0x87;
-  end[3] = 0x88;
+// Places the mouse ot the centre of the next square
+void centre() {
+  for (int i = 0; i < 3; i++) {scan(i);}
+  // Check gyroscope
 
-  while (!in(pos, end, 4)) {
-    
+  // Calculates distances to each wall
+  float phi, cos_phi, tempw, tempn, tempe, Sw, Sn, Se, h, projected;
+  phi = Gz - orient * HALF_PI;
+  cos_phi = cos(phi);
+  tempw = D[0] * cos_phi;
+  tempn = D[1] * cos_phi;
+  tempe = D[2] * cos_phi;
+  Sw = tempw - floor(tempw);
+  Sn = tempn - floor(tempn);
+  Se = tempe - floor(tempe);
+  h = Sn + 0.5;
+  projected = h * tan(phi) + Sw - Se;
+
+  // Course-corrects the mouse if its going too far right
+  if (projected > 0.1) {
+    float w, s, theta, inv_r;
+    w = abs(Sw - Se);
+    s = sqrt(h*h + w*w);
+    theta = acos((h*cos_phi + h - w*sin(Gz))/s) + acos(-h/s) + PI;
+    if (theta > 2 * PI) theta -= 2 * PI;
+    inv_r = (1 + cos_phi + 2 * cos(theta)) / w;
+
+    // Output MOTOR_OUTPUT to right motor
+    // Output MOTOR_OUTPUT - inv_r to left motor
+    while (phi > theta || phi < PI) {
+      delayMicroseconds(10);
+      // Check gyroscope
+    }
+    // Output MOTOR_OUTPUT to left motor
+    // Output MOTOR_OUTPUT - inv_r to right motor
+    while (phi > PI) {
+      delayMicroseconds(10);
+      // Check gyroscope
+    }
+    // Output MOTOR_OUTPUT to right motor
   }
+
+  // Course-corrects the mouse if its going too far left
+  else if (projected < -0.1) {
+    float w, s, theta, inv_r;
+    w = abs(Sw - Se);
+    s = sqrt(h*h + w*w);
+    theta = acos((h*cos_phi + h - w*sin(Gz))/s) + acos(-h/s) + PI;
+    if (theta > 2 * PI) theta -= 2 * PI;
+    inv_r = (1 + cos_phi + 2 * cos(theta)) / w;
+
+    // Output MOTOR_OUTPUT to left motor
+    // Output MOTOR_OUTPUT - inv_r to right motor
+    while (phi < theta || phi > PI) {
+      delay(10);
+      // Check gyroscope
+      phi = Gz - orient * HALF_PI;
+    }
+    // Output MOTOR_OUTPUT to right motor
+    // Output MOTOR_OUTPUT - inv_R to left motor
+    while (phi < PI) {
+      delay(10);
+      // Check gyroscope
+      phi = Gz - orient * HALF_PI;
+    }
+    // Output MOTOR_OUTPUT to right motor
+  }
+
+  // Brings the mouse directly to the centre of the next square along a straight line
+  else {
+    // Output MOTOR_OUTPUT to right motor
+    // Output MOTOR_OUTPUT to left motor
+    if (Sn > 0.5) {
+      while (Sn > 0.4) {
+        delay(10);
+        scan(1);
+        tempn = D[1] * cos_phi;
+        Sn = tempn - floor(tempn);
+      }
+    }
+    while (Sn < 0.5) {
+      delay(10);
+      scan(1);
+      tempn = D[1] * cos_phi;
+      Sn = tempn - floor(tempn);
+    }
+    while (Sn > 0.5) {
+      delay(10);
+      scan(1);
+      tempn = D[1] * cos_phi;
+      Sn = tempn - floor(tempn);
+    }
+  }
+}
+
+/*------------------------------- Main -------------------------------*/
+void setup() {
+  Serial.begin(9600);
+
+  // Initialises variables
+  for (int y = 0; y < 16; ++y) {
+    for (int x = 0; x < 16; ++x) {
+      walls[y][x] = 0x00;
+    }
+  }
+  for (int i = 0; i < 3; ++i) {
+    trig[i] = 2*i + 2;
+    echo[i] = 2*i + 3;
+  }
+
+  // Inputs and outputs
+  for (int i = 0; i < 3; ++i) {
+    pinMode(trig[i], OUTPUT);
+    pinMode(echo[i], INPUT);
+  }
+}
+
+void loop() {
+  
 }
